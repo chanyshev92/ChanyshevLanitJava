d12e966e203bc1e09bad464437ff142c6560c36f
Рустам Чанышев <chanyshev92@gmail.com>
2022-06-15 21:07:28 +0500

    some changes 5

--- src/test/java/PositiveCalculatorTest.java
+++ src/test/java/PositiveCalculatorTest.java
@@ -9,23 +9,26 @@ public class PositiveCalculatorTest {
     @DataProvider
     public Object[][] positiveData(){
         return new Object[][]{
+                {"+","0.2","0.3",0.5},
+                {"/","1.25","5",0.25},
+                {"-", "2.0","1.1",0.8999999999999999},
                 {"+","1","2",3},
                 {"-","1","1",0},
                 {"*","5","5",25},
                 {"/","12","2",6},
                 {"-",String.valueOf(Integer.MAX_VALUE-0.01),"123",Integer.MAX_VALUE-123.01},
                 {"+",String.valueOf(Integer.MAX_VALUE-123),"122.99",Integer.MAX_VALUE-0.01},
-                {"+",String.valueOf(Integer.MIN_VALUE+1),"123",Integer.MIN_VALUE+124},
-                {"+","0.2","0.3",0.5},
-                {"/","1.25","5",0.25},
-                {"-", "2.0","1.1",0.8999999999999999}
+                {"+",String.valueOf(Integer.MIN_VALUE+1),"123",Integer.MIN_VALUE+124}
+
         };
     }
 
     @Test(dataProvider="positiveData")
     public void positiveTest(String operator,String arg1, String arg2, double result){
         try {
-            Assert.assertEquals(String.valueOf(result), Calculator.execute(new String[]{operator,arg1,arg2}),"not equals");
+            //Assert.assertEquals(String.valueOf(result), Calculator.execute(new String[]{operator,arg1,arg2}),"not equals");
+            //Assert.assertEquals(Calculator.execute(new String[]{operator, arg1, arg2}),String.format("%.3f", result));
+            Assert.assertEquals(Calculator.execute(new String[]{operator,arg1,arg2}),String.valueOf(result));
 
         }catch (CalculatorException e){
             e.printStackTrace();
